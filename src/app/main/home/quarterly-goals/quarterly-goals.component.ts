596d87ba6c5c28c3530f67c27c912a903602ae0c
Daisy Austin <daustin1@ucsc.edu>
2026-07-28 09:58:31 -0700

    feat: implement Quarterly Goals Header (#4)

--- src/app/main/home/quarterly-goals/quarterly-goals.component.ts
+++ src/app/main/home/quarterly-goals/quarterly-goals.component.ts
@@ -4,6 +4,7 @@ import { User } from 'src/app/core/store/user/user.model';
 import { AuthStore } from 'src/app/core/store/auth/auth.store';
 import { BatchWriteService, BATCH_WRITE_SERVICE } from 'src/app/core/store/batch-write.service';
 import { QuarterlyGoalsItemComponent } from './quarterly-goals-item/quarterly-goals-item.component';
+import { QuarterlyGoalsHeaderComponent } from './quarterly-goals-header/quarterly-goals-header.component';
 import { QuarterlyGoal } from '../../../core/store/quarterly-goal/quarterly-goal.model';
 import { QuarterlyGoalData } from '../home.model';
 import { Hashtag } from '../../../core/store/hashtag/hashtag.model';
@@ -16,6 +17,7 @@ import { MatSnackBar } from '@angular/material/snack-bar';
   animations: QuarterlyGoalsAnimations,
   standalone: true,
   imports: [ QuarterlyGoalsItemComponent,
+             QuarterlyGoalsHeaderComponent
   ],
 })
 export class QuarterlyGoalsComponent implements OnInit {
@@ -69,6 +71,17 @@ export class QuarterlyGoalsComponent implements OnInit {
     );
   }
   
+  modalClicked(newModalState: boolean) {
+    this.snackBar.open(
+      'Modal button clicked.',
+      '',
+      {
+        duration: 3000,
+        verticalPosition: 'bottom',
+        horizontalPosition: 'center',
+      },
+    );
+  }
   // --------------- OTHER -------------------------------
 
   constructor(
