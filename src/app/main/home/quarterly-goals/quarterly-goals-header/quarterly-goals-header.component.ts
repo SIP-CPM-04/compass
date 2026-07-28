fa58c6b3cc812b51a07ccfd3455e7dd251cdff8f
daustin1 <daustin1@ucsc.edu>
2026-07-28 09:57:49 -0700

    feat: implement Quarterly Goals Header

--- src/app/main/home/quarterly-goals/quarterly-goals-header/quarterly-goals-header.component.ts
+++ src/app/main/home/quarterly-goals/quarterly-goals-header/quarterly-goals-header.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit, ChangeDetectionStrategy, input, output, inject, WritableSignal, Signal, signal, computed, Inject, Injector } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy, input, output, inject, WritableSignal, Signal, signal, computed, Inject, Injector, OutputEmitterRef } from '@angular/core';
 import { QuarterlyGoalsHeaderAnimations } from './quarterly-goals-header.animations';
 import { User } from 'src/app/core/store/user/user.model';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { interval, map } from 'rxjs';
 import { AuthStore } from 'src/app/core/store/auth/auth.store';
-import { BatchWriteService, BATCH_WRITE_SERVICE } from 'src/app/core/store/batch-write.service';
-
+import { DatePipe } from '@angular/common';
 @Component({
   selector: 'app-quarterly-goals-header',
   templateUrl: './quarterly-goals-header.component.html',
@@ -11,11 +12,12 @@ import { BatchWriteService, BATCH_WRITE_SERVICE } from 'src/app/core/store/batch
   changeDetection: ChangeDetectionStrategy.OnPush,
   animations: QuarterlyGoalsHeaderAnimations,
   standalone: true,
-  imports: [
-  ],
+  imports: [DatePipe],
 })
 export class QuarterlyGoalsHeaderComponent implements OnInit {
   readonly authStore = inject(AuthStore);
+  modal: OutputEmitterRef<boolean> = output<boolean>();
+
   // --------------- INPUTS AND OUTPUTS ------------------
 
   /** The current signed in user. */
@@ -25,16 +27,35 @@ export class QuarterlyGoalsHeaderComponent implements OnInit {
 
   /** Loading icon. */
   loading: WritableSignal<boolean> = signal(false);
+  onModal: boolean = false;
 
   // --------------- COMPUTED DATA -----------------------
+  time: Signal<Date> = toSignal(
+    interval(1000).pipe(
+      map(() => new Date())
+    ),
+    { initialValue: new Date() }
+  );
 
+  season: Signal<string> = computed(() => {
+    const currentMonth = this.time().getMonth();
+    switch (~~((currentMonth+1) / 3)) {
+      case 0: return 'Winter';
+      case 1: return 'Spring';
+      case 2: return 'Summer';
+      case 3: return 'Autumn';
+      default: return 'Error';
+    }
+  });
   // --------------- EVENT HANDLING ----------------------
-
+  modalClicked() {
+    this.onModal = !this.onModal;
+    this.modal.emit(this.onModal);
+  }
   // --------------- OTHER -------------------------------
 
   constructor(
     private injector: Injector,
-    @Inject(BATCH_WRITE_SERVICE) private batch: BatchWriteService,
   ) { }
 
   // --------------- LOAD AND CLEANUP --------------------
